922cf2fde009b115df3dd6ac00e0a62e93113f82
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-03-11 11:29:32 -0500

    Began work on '/api/notes' routing in server

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
-const http = require("http")
-const express = require("express")
-const path = require("path")
-const fs = require("fs")
+//Getting dependencies
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-let app = express()
-let PORT = process.env.PORT || 8080
+//Initializing Express and our PORT
+let app = express();
+//Also leaving the logic in for when we deploy to Heroku
+let PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+//Routes
 
-app.get("/", function (req, res){
-    res.sendFile(path.join(__dirname, "index.html"))
-})
+//Home page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+//Where we take our notes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+//Where our saved notes live! (For now)
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "db.json"));
+});
+//Posting a new note to our "database"
+app.post("/api/notes", function(req, res) {
+  let newNote = req.body;
 
-app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "notes.html"))
-})
+  console.log(newNote);
+});
+//// A function for saving a note to the db
+// let saveNote = function(note) {
+//     return $.ajax({
+//       url: "/api/notes",
+//       data: note,
+//       method: "POST"
+//     });
+//   };
 
+app.listen(PORT, function() {
+  console.log(`Listening at: http://localhost:${PORT}`);
+});
